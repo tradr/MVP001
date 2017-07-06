d924b627818ad7e4a70e428daadc6c5c21d676a7
Ev Ooi <hello@evthedev.com>
2017-07-06 23:34:34 +0800

    Modified nav links

--- drugs.php
+++ drugs.php
@@ -9,12 +9,12 @@
       <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
-      <a class="navbar-brand" href="#">TRADR</a>
+      <a class="navbar-brand" href="/">TRADR</a>
 
       <div class="collapse navbar-collapse" id="navbarsExampleDefault">
         <ul class="navbar-nav mr-auto">
           <li class="nav-item active">
-            <a class="nav-link" href="#">Buy stuff<span class="sr-only">(current)</span></a>
+            <a class="nav-link" href="/">Buy stuff<span class="sr-only">(current)</span></a>
           </li>
           <li class="nav-item">
             <a class="nav-link" href="#">Sell stuff</a>
